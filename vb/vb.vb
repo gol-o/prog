3d69c83e580a289383bc63c30b1494c5bd6f512a
Guido Otto Lorch <134075514+gol-o@users.noreply.github.com>
2023-05-29 09:19:51 +0200

    Update vb.vb

--- vb/vb.vb
+++ vb/vb.vb
@@ -1,3 +1,23 @@
+employee = inputbox( "Name and Department" )
+
+' check for delimiter
+pos = instr( employee, ";" )
+if not pos then
+    pos = instr( employee, "," )
+    if not pos then 
+        pos = instr( employee, " " )
+        if not pos then
+            msgbox "Delimiter not included" 
+        end if
+    end if
+end if
+
+'filename = left( employee, pos - 1 )
+wscript.echo filename
+wscript.echo not 0, not 1, not 9
+
+
+
 Imports System.Console
 
 ' Project Settings: Startobject 
