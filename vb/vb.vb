72462f119de6646c9986c96766264dda274cf062
Guido Otto Lorch <134075514+gol-o@users.noreply.github.com>
2023-05-29 11:22:27 +0200

    Update vb.vb

--- vb/vb.vb
+++ vb/vb.vb
@@ -1,23 +1,35 @@
+'------------------------------------------------------------------------------- 
+' Windows Scripting Host
+'------------------------------------------------------------------------------- 
 employee = inputbox( "Name and Department" )
 
 ' check for delimiter
 pos = instr( employee, ";" )
-if not pos then
+if not cbool( pos ) then ' try different delimiter
     pos = instr( employee, "," )
-    if not pos then 
+    if not cbool( pos ) then
         pos = instr( employee, " " )
-        if not pos then
-            msgbox "Delimiter not included" 
+        if not cbool( pos ) then
+            msgbox "delimiter not included"
         end if
     end if
 end if
 
-'filename = left( employee, pos - 1 )
-wscript.echo filename
-wscript.echo not 0, not 1, not 9
+filename = left( employee, pos - 1 )
+desktop  = createobject( "wscript.shell" ).specialfolders( "desktop" )
+set file = createobject( "scripting.filesystemobject" ).createtextfile( desktop + "/" + filename )
 
+file.writeline( "employee record, timestamp: " + formatdatetime( now, vblongtime ) )
+file.writeline( "---------------------------------------" )
+file.writeline( "name: " + filename ) 
+file.writeline( "dep : " + mid( employee, pos + 1 ) ) 
+file.close( )
 
+msgbox "record written"
 
+'------------------------------------------------------------------------------- 
+    ' System Framework
+    '------------------------------------------------------------------------------- 
 Imports System.Console
 
 ' Project Settings: Startobject 
